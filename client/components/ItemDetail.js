4c3c4c9dc7a8bee58f5126aedbd31abf3e22cc16
laurimil <laurimilehto@gmail.com>
2018-01-23 17:07:12 +0200

    material itemdetail

--- client/components/ItemDetail.js
+++ client/components/ItemDetail.js
@@ -15,11 +15,25 @@ class ItemDetail extends Component {
 
     return (
       <div>
+      
+        <div class="mdl-card mdl-shadow--2dp">
+          <div class="mdl-card__title mdl-card--expand">
+            <h2 class="mdl-card__title-text">{title}</h2>
+          </div>
+          <div class="mdl-card__supporting-text">
+            <p>{description}</p>
+            <p>{maker},{year}</p>
+            <p>{price}</p>
+          </div>
+          <div class="mdl-card__actions mdl-card--border">
+            <a class="mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect">
+              View Updates
+            </a>
+          </div>
+        </div>
 
-        <h3>{title}</h3>
-        <p>{description}</p>
-        <p>{maker},{year}</p>
-        <p>{price}</p>
+       
+      
 
       </div>
     );
