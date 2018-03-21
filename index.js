10407bc8081b4a4b4fd32cab61729ace4416710c
laurimil <lauriml@metropolia.fi>
2018-03-21 14:49:12 +0200

    dotenv run deviin

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// require('dotenv').config();
-
 const app = require('./server/server');
 
-app.listen(process.env.PORT, () => {
-  console.log('Listening');
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Listening on port '+ process.env.PORT);
 });
