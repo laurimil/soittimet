02de0905d91c7efa6bd647f386112b52312cd376
laurimil <lauriml@metropolia.fi>
2018-03-22 09:51:49 +0200

    dotenv run dev

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-// require('dotenv').config();
-
 const app = require('./server/server');
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Listening');
 });
